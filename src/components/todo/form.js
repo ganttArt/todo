c7189ad349d4149ab0376f43dce127a7367035bc
Chris Gantt <gantt.art@gmail.com>
2021-06-01 13:10:08 -1000

    Updated styling on form

--- src/components/todo/form.js
+++ src/components/todo/form.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Form, Button } from 'react-bootstrap';
 
 const TodoForm = (props) => {
   const [item, setItem] = useState({});
@@ -6,6 +7,7 @@ const TodoForm = (props) => {
   const handleInputChange = e => {
     const updatedItem = item;
     updatedItem[e.target.name] = e.target.value;
+    console.log(item);
     setItem(updatedItem);
   };
 
@@ -17,28 +19,26 @@ const TodoForm = (props) => {
   };
 
   return (
-    <>
+    <div id='form-container'>
       <h3>Add Item</h3>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <span>To Do Item</span>
-          <input
-            name="text"
-            placeholder="Add To Do List Item"
-            onChange={handleInputChange}
-          />
-        </label>
-        <label>
-          <span>Difficulty Rating</span>
-          <input defaultValue="1" type="range" min="1" max="5" name="difficulty" onChange={handleInputChange} />
-        </label>
-        <label>
-          <span>Assigned To</span>
-          <input type="text" name="assignee" placeholder="Assigned To" onChange={handleInputChange} />
-        </label>
-        <button>Add Item</button>
-      </form>
-    </>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="text">
+          <Form.Label>To Do Item</Form.Label>
+          <Form.Control name='text' type="text" placeholder="Item Details" onChange={handleInputChange}/>
+        </Form.Group>
+        <Form.Group controlId="assigneecg">
+          <Form.Label>Assigned To</Form.Label>
+          <Form.Control type="text" name="assignee" placeholder="Assignee Name" onChange={handleInputChange}/>
+        </Form.Group>
+        <Form.Group controlId="difficultycg">
+          <Form.Label>Difficulty Rating</Form.Label>
+          <Form.Control type="range" defaultValue="1" min="1" max="5" onChange={handleInputChange}/>
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Add item
+        </Button>
+      </Form>
+    </div>
   )
 }
 
