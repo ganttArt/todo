43554680c2ea17e684150322fa131b97a12d7d98
Chris Gantt <gantt.art@gmail.com>
2021-06-01 10:04:00 -1000

    Updated Main component from class to function based.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 
 import App from './app.js';
 
-class Main extends React.Component {
-  render() {
-    return <App />;
-  }
+function Main() {
+  return (
+    <App />
+  )
 }
 
 const rootElement = document.getElementById('root');
