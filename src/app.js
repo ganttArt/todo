c754d81c843a2b66c1cc40de843e535e807b2d46
Chris Gantt <gantt.art@gmail.com>
2021-06-01 10:06:43 -1000

    Updated app component from class to function based.

--- src/app.js
+++ src/app.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 import ToDo from './components/todo/todo.js';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <>
-        <ToDo />
-      </>
-    );
-  }
+function App() {
+  return (
+    <>
+      <ToDo />
+    </>
+  )
 }
+
+export default App;
