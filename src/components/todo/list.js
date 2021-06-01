50b85a8dee0eadb41dcb7a4b61049dd28ebdef6e
Chris Gantt <gantt.art@gmail.com>
2021-06-01 12:07:22 -1000

    Used react-bootstrap and sass to style the TodoList component

--- src/components/todo/list.js
+++ src/components/todo/list.js
@@ -1,19 +1,20 @@
 import React from 'react';
+import { ListGroup } from 'react-bootstrap';
 
 function TodoList(props) {
   return (
-    <ul>
-        {props.list.map(item => (
-          <li
-            className={`complete-${item.complete.toString()}`}
-            key={item._id}
-          >
-            <span onClick={() => props.handleComplete(item._id)}>
-              {item.text}
-            </span>
-          </li>
-        ))}
-      </ul>
+    <ListGroup>
+      {props.list.map(item => (
+        <ListGroup.Item
+          className={`complete-${item.complete.toString()}`}
+          key={item._id}
+          onClick={() => props.handleComplete(item._id)}
+          variant={item.complete ? 'success' : 'danger'}
+        >
+          {item.text}
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
   )
 }
 
