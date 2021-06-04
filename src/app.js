2243da4e584384dc8cc0dfc6b15cd718a4080a5f
Chris Gantt <gantt.art@gmail.com>
2021-06-03 15:45:19 -1000

    Created app level context. Todo list changed to display todos based on displayCompleted state in SettingsContext

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import NavBar from './components/navbar';
-
 import ToDo from './components/todo/todo.js';
+import SettingsContext from './context/settings';
 
 function App() {
   return (
-    <>
+    <SettingsContext>
       <NavBar />
       <ToDo />
-    </>
+    </SettingsContext>
   )
 }
 
