83cfeaafb007e538a9cb7596a8e55e25b9120d7d
Chris Gantt <gantt.art@gmail.com>
2021-06-03 15:56:57 -1000

    Updated list of todos to be sorted by difficulty level, which is being stored in SettingsContext.

--- src/components/todo/list.js
+++ src/components/todo/list.js
@@ -8,54 +8,57 @@ import { If, Then, Else } from 'react-if';
 function TodoList(props) {
 
   const context = useContext(SettingsContext);
+  const sortingField = context.sortBy;
 
   return (
     <ListGroup>
-      {props.list.map(item => (
-        <If condition={context.displayCompleted}>
-          <Then>
-            <Card key={item._id}>
-              <Card.Header>
-                <Badge 
-                  variant={item.complete ? 'danger' : 'success'}
-                  className={`complete-${item.complete.toString()}`}
-                  onClick={() => props.handleComplete(item._id)}
-                >
-                  {item.complete ? 'Completed' : 'Pending'}
-                </Badge>{' '}
-                <div id='assignee'>{item.assignee}</div>
-                <Button onClick={() => props.handleDelete(item._id)}>X</Button>
-              </Card.Header>
-              <Card.Body>
-                <Card.Text>{item.text}</Card.Text>
-                <div id='difficulty'>Difficulty: {item.difficulty}</div>
-              </Card.Body>
-            </Card>
-          </Then>
-          <Else>
-            <If condition={!item.complete}>
-              <Then>
-                <Card key={item._id}>
-                  <Card.Header>
-                    <Badge 
-                      variant={item.complete ? 'danger' : 'success'}
-                      className={`complete-${item.complete.toString()}`}
-                      onClick={() => props.handleComplete(item._id)}
-                    >
-                      {item.complete ? 'Completed' : 'Pending'}
-                    </Badge>{' '}
-                    <div id='assignee'>{item.assignee}</div>
-                    <Button onClick={() => props.handleDelete(item._id)}>X</Button>
-                  </Card.Header>
-                  <Card.Body>
-                    <Card.Text>{item.text}</Card.Text>
-                    <div id='difficulty'>Difficulty: {item.difficulty}</div>
-                  </Card.Body>
-                </Card>
-              </Then>
-            </If>
-          </Else>
-        </If>
+      {props.list
+        .sort((a, b) => Number(a[sortingField]) > Number(b[sortingField]) ? -1 : 1)
+        .map(item => (
+          <If condition={context.displayCompleted}>
+            <Then>
+              <Card key={item._id}>
+                <Card.Header>
+                  <Badge 
+                    variant={item.complete ? 'danger' : 'success'}
+                    className={`complete-${item.complete.toString()}`}
+                    onClick={() => props.handleComplete(item._id)}
+                  >
+                    {item.complete ? 'Completed' : 'Pending'}
+                  </Badge>{' '}
+                  <div id='assignee'>{item.assignee}</div>
+                  <Button onClick={() => props.handleDelete(item._id)}>X</Button>
+                </Card.Header>
+                <Card.Body>
+                  <Card.Text>{item.text}</Card.Text>
+                  <div id='difficulty'>Difficulty: {item.difficulty}</div>
+                </Card.Body>
+              </Card>
+            </Then>
+            <Else>
+              <If condition={!item.complete}>
+                <Then>
+                  <Card key={item._id}>
+                    <Card.Header>
+                      <Badge 
+                        variant={item.complete ? 'danger' : 'success'}
+                        className={`complete-${item.complete.toString()}`}
+                        onClick={() => props.handleComplete(item._id)}
+                      >
+                        {item.complete ? 'Completed' : 'Pending'}
+                      </Badge>{' '}
+                      <div id='assignee'>{item.assignee}</div>
+                      <Button onClick={() => props.handleDelete(item._id)}>X</Button>
+                    </Card.Header>
+                    <Card.Body>
+                      <Card.Text>{item.text}</Card.Text>
+                      <div id='difficulty'>Difficulty: {item.difficulty}</div>
+                    </Card.Body>
+                  </Card>
+                </Then>
+              </If>
+            </Else>
+          </If>
       ))
       }
     </ListGroup >
